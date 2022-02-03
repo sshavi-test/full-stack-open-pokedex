f3f4674d95c732687a3a8730fa53f964771a2256
sshavi-test <98963103+sshavi-test@users.noreply.github.com>
2022-02-03 19:09:38 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Heroku dynamically sets a port
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
+  
 });
